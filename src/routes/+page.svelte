58818eedb46247962ae6e242aa30a0eefd1ebb8b
Dāvis Karpovskis <63516519+IceSalesman@users.noreply.github.com>
2023-09-19 10:16:09 +0000

    basic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="js">
+    let count = 0;
+
+
+</script>
+
+<title>Volejbols</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<style lang="css">
+
+    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    }
+
+</style>
---
+++ src/routes/resourses/LoginForm.svelte
@@ -0,0 +1,23 @@
+<script>
+
+
+
+</script>
+
+<div class="login">
+    Pieslēgties
+</div>
+
+<div class="signup">
+    Reģistrēties
+</div>
+
+
+
+
+
+<style>
+
+
+
+</style>
