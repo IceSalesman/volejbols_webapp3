c70cbd6e34f216f23ed5d365745662c1a13a5415
Dāvis Karpovskis <63516519+IceSalesman@users.noreply.github.com>
2023-09-24 09:40:26 +0000

    beidzot kautkas

--- src/routes/components/LoginForm.svelte
+++ src/routes/components/LoginForm.svelte
@@ -3,33 +3,33 @@
 
 
 </script>
+<main>
+   <div class="login">
+      {label}
+   </div>
 
-<div class="login">
-    {label}
-</div>
+   <input type="radio" name="slide" id="login" checked>
+   <label for="login" class="slide login">{label}</label>
 
-<input type="radio" name="slide" id="login" checked>
-<label for="login" class="slide login">{label}</label>
-
-<form>
-    <div class="field">
-        <input type="text" placeholder="E-pasta adrese" required>
-     </div>
-     <div class="field">
-        <input type="password" placeholder="Parole" required>
-     </div>
-     <div class="pass-link">
-        <a href="#">Aizsmirsi paroli?</a>
-     </div>
-     <div class="field btn">
-        <div class="btn-layer"></div>
-        <input type="submit" value="Pieslēgties">
-     </div>
-     <div class="signup-link">
-       <a href="">Nēesi reģistrējies? </a>
-     </div>
-</form>
-
+   <form>
+      <div class="field">
+         <input type="text" placeholder="E-pasta adrese" required>
+      </div>
+      <div class="field">
+         <input type="password" placeholder="Parole" required>
+      </div>
+      <div class="pass-link">
+         <a href="#top">Aizsmirsi paroli?</a>
+      </div>
+      <div class="field btn">
+         <div class="btn-layer"></div>
+         <input type="submit" value="Pieslēgties">
+      </div>
+      <div class="signup-link">
+         <a href="https://www.nva.gov.lv/lv/bezdarbnieka-statusa-iegusana?utm_source=https%3A%2F%2Fwww.google.com%2F">Neesi reģistrējies? </a>
+      </div>
+   </form>
+</main>
 
 
 
