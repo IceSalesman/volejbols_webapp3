105d72839293b50b25a3d8fed03403ebac086114
Edzonsxd <zuga.eduards@gmail.com>
2023-10-23 21:52:38 +0300

    Login lapa noformateta ar proporcijam nevis ar zirgu matemātiku

--- src/lib/components/LoginSignup/LoginForm.svelte
+++ src/lib/components/LoginSignup/LoginForm.svelte
@@ -10,45 +10,47 @@
 </script>
 
 <form method="post" action="" class="h-full">
-	<div class="h-full flex flex-col justify-evenly items-center">
-		<div class="w-[330px] border mx-10 p-1 rounded hover:border-sky-500 focus-within:border-sky-500">
-			<input
-				type="email"
-				name="email"
-				class = "outline-none"
-				placeholder={inputEmail}
-				required
-			/>
-		</div>
-		<div class="w-max-[330px] flex border p-1 rounded group hover:border-sky-500 focus-within:border-sky-500">
-			<input
-				type={show ? 'text' : 'password'}
-				name="password"
-				placeholder={inputPass}
-				class="outline-none"
-				required
-			/>
-			<!-- Es hz kā pareizi ievadīt svg failu, img neštirī -->
-			<button id="eye" on:click|preventDefault={() => show = !show}>
-				{#if show}
-					<ShowEye />
-				{:else}
-					<HideEye />
-				{/if}
-			</button>
-		</div>
-		<div>
-			<a
-				class="flex no-underline hover:underline text-[#4137ab] justify-center items-center p-1"
-				href="https://www.nva.gov.lv/lv/bezdarbnieka-statusa-iegusana">Aizmirsi paroli?</a
-			>
-		</div>
-		<div class="flex flex-col items-center">
-			<button
-				type="submit"
-				class="p-1 px-4 items-center cursor-pointer bg-gray-100 rounded text-lg font-bold text-sky-500 hover:bg-gray-200"
-				>Pieslēgties</button
-			>
+	<div class="w-full h-full flex justify-center">
+		<div class="w-4/5 h-full flex flex-col justify-evenly">
+			<div class="formInputDiv">
+				<input
+					type="email"
+					name="email"
+					class = "outline-none w-full"
+					placeholder={inputEmail}
+					required
+				/>
+			</div>
+			<div class="formInputDiv flex flex-row">
+				<input
+					type={show ? 'text' : 'password'}
+					name="password"
+					placeholder={inputPass}
+					class="outline-none w-11/12"
+					required
+				/>
+				<!-- Es hz kā pareizi ievadīt svg failu, img neštirī -->
+				<button id="eye" on:click|preventDefault={() => show = !show}>
+					{#if show}
+						<ShowEye />
+					{:else}
+						<HideEye />
+					{/if}
+				</button>
+			</div>
+			<div>
+				<a
+					class="flex no-underline hover:underline text-[#4137ab] justify-center items-center p-1"
+					href="https://www.nva.gov.lv/lv/bezdarbnieka-statusa-iegusana">Aizmirsi paroli?</a
+				>
+			</div>
+			<div class="flex flex-col items-center">
+				<button
+					type="submit"
+					class="p-1 px-4 items-center cursor-pointer bg-gray-100 rounded text-lg font-bold text-sky-500 hover:bg-gray-200"
+					>Pieslēgties</button
+				>
+			</div>
 		</div>
 	</div>
 </form>
