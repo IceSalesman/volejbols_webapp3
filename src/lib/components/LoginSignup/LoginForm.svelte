59783937c7a19056895c802ed3f3ba4f3da7ebcd
Eduards Zuga <zuga.eduards@gmail.com>
2023-10-23 14:31:48 +0300

    Actiņa tevi redz

--- src/lib/components/LoginSignup/LoginForm.svelte
+++ src/lib/components/LoginSignup/LoginForm.svelte
@@ -1,36 +1,52 @@
 <script lang="ts">
+	import HideEye from "../SVGicons/HideEye.svelte";
+	import ShowEye from "../SVGicons/ShowEye.svelte";
+
+
 	let inputEmail = 'E-pasta adrese';
 	let inputPass = 'Parole';
+
+	let show = false;
 </script>
 
 <form method="post" action="" class="h-full">
 	<div class="h-full flex flex-col justify-evenly items-center">
-      <div>
-         <input
-            type="email"
-            name="email"
-            class="border p-1 rounded"
-            placeholder={inputEmail}
-            required
-         />
-      </div>
-      <div class="">
-         <input
-            type="password"
-            name="password"
-            class="border p-1 rounded"
-            placeholder={inputPass}
-            required
-         />
-      </div>
-      <div>
-         <a
-            class="flex no-underline hover:underline text-[#4137ab] justify-center items-center p-1"
-            href="https://www.nva.gov.lv/lv/bezdarbnieka-statusa-iegusana">Aizmirsi paroli?</a
-         >
-      </div>
-      <div class="flex flex-col items-center">
-         <button type="submit" class="p-1 px-4 items-center cursor-pointer bg-gray-100 rounded text-lg font-bold text-sky-500 hover:bg-gray-200">Pieslēgties</button>
-      </div>
-   </div>
+		<div class="border p-1 rounded ">
+			<input
+				type="email"
+				name="email"
+				placeholder={inputEmail}
+				required
+			/>
+		</div>
+		<div class="flex border p-1 rounded">
+			<input
+				type={show ? 'text' : 'password'}
+				name="password"
+				placeholder={inputPass}
+				required
+			/>
+			<!-- Es hz kā pareizi ievadīt svg failu, img neštirī -->
+			<button id="eye" on:click|preventDefault={() => show = !show}>
+				{#if show}
+					
+					<ShowEye />
+				{:else}
+					<HideEye />
+				{/if}
+		</div>
+		<div>
+			<a
+				class="flex no-underline hover:underline text-[#4137ab] justify-center items-center p-1"
+				href="https://www.nva.gov.lv/lv/bezdarbnieka-statusa-iegusana">Aizmirsi paroli?</a
+			>
+		</div>
+		<div class="flex flex-col items-center">
+			<button
+				type="submit"
+				class="p-1 px-4 items-center cursor-pointer bg-gray-100 rounded text-lg font-bold text-sky-500 hover:bg-gray-200"
+				>Pieslēgties</button
+			>
+		</div>
+	</div>
 </form>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <form action="">
+        <div class="flex items-center">
+            <div class="m-2">
+                <label for="name">Vārds</label>
+                <input id="name" type="text" placeholder="blank" class="border" required/>
+            </div>
+            <div class="m-2">
+                <label for="name">Otrais vārds?</label>
+                <input id="second name" type="text" placeholder="blank" class="border"/>
+            </div>
+            <div class="m-2">
+                <label for="name">Uzvārds</label>
+                <input id="last name" type="text" placeholder="blank" class="border" required/>
+            </div>
+        </div>
+        <div class="m-2">
+            <label for="email">E-pasts</label>
+            <input id="email" type="email" placeholder="blank" class="border" required/>
+        </div>
+        <div class="m-2">
+            <label for="password">Parole</label>
+            <input id="password" type="password" placeholder="blank" class="border" required/>
+        </div>
+        <div class="m-2">
+            <label for="repeatPassword">Atkārtot paroli</label>
+            <input id="repeatPassword" type="password" placeholder="blank" class="border" required/></div>
+    </form>
+</main>
