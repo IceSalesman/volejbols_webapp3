07819cbe00cee46ac53c04a8cd8783ea49fe405b
Edzonsxd <zuga.eduards@gmail.com>
2023-11-05 17:52:46 +0200

    Kkas nestiri ar logu mainu

--- src/lib/components/LoginSignup/EmailVerificationInput.svelte
+++ src/lib/components/LoginSignup/EmailVerificationInput.svelte
@@ -1,37 +1,24 @@
 <script lang="ts">
+	import { enhance } from "$app/forms";
 	import { otpSent } from "../../../stores";
-	
+
 	let inputEmail = 'E-pasta adrese';
 
-	const handleSubmit = () => {
-		otpSent.set(true)
-	}
-	
 </script>
 
-
-
-<form method="post" on:submit|preventDefault={handleSubmit} class="h-full">
+<!-- Nepārmainās logs uz verifikācijas apstiprināšanas logu -->
+<form method="post" on:submit|preventDefault use:enhance action="?/sendVerificationCode" class="h-full">
 	<div class="h-full flex flex-col justify-evenly items-center">
 		<div class="w-4/5 h-full flex flex-col justify-evenly">
 			<p class="text-center">
-				<strong>Sveiks volejbola entuziast!</strong> <br /> 
+				<strong>Sveiks volejbola entuziast!</strong> <br />
 				Ievadi savu e-pastu, lai uzsāktu reģistrāciju!
 			</p>
 			<div class="formInputDiv">
-				<input
-					type="email"
-					name="email"
-					class="outline-none w-full"
-					placeholder={inputEmail}
-				/>
+				<input type="email" name="email" class="outline-none w-full" placeholder={inputEmail} />
 			</div>
 			<div class="flex flex-col items-center">
-				<button
-					type="submit"
-					class="loginPageSubmitButton"
-					>Reģistrēties
-				</button>
+				<button type="submit" class="loginPageSubmitButton">Reģistrēties </button>
 			</div>
 		</div>
 	</div>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,43 +1,16 @@
-import transporter from "$lib/utils/email"
-import { EMAIL } from "$env/static/private"
+import { sendVerificationCode } from "$lib/emails/email"
+import { otpSent } from "../stores";
+
 
 export const actions = {
-    default: async({request}) => {
-        try {
-            const formData = await request.formData();
-            const toEmail = formData.get("email");
-            let html = `<h2><strong>Hi!</h2><pre>${toEmail}</pre>`;
+    sendVerificationCode: async ({ request }) => {
+        const formData = await request.formData();
+        const toEmail = String(formData.get("email"));
+        const testValue = "Test123"
 
-            const message = {
-                from: {
-                    name: "Verifikācijas kods",
-                    address: EMAIL
-                },
-                to: toEmail,
-                subject: "test",
-                text: "test",
-                html: html,
-            };
+        await sendVerificationCode(toEmail, testValue);
 
-            const sendEmail = async (message: any) => {
-                await new Promise((resolve, reject) => {
-                    transporter.sendMail(message, (err, info) => {
-                        if (err) {
-                            console.error(err);
-                            reject(err);
-                        } else {
-                            resolve(info);
-                        }
-                    });
-                });
-            };
-
-            await sendEmail(message);
-            return {
-                success: "Email is sent",
-            };
-        } catch (error) {
-            console.error(error);
-        }
+        otpSent.set(true);
+        console.log("optSent:" + otpSent)
     }
 }
