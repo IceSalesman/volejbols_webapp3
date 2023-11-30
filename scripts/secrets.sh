c4749c601acb9e1351eb69ac7f227746049a8f5a
Dāvis Karpovskis <63516519+IceSalesman@users.noreply.github.com>
2023-11-30 09:44:26 +0000

    ahaha

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-export EMAIL_PASSWORD=parole
-export EMAIL=epasts
+TEST=$(grep -oP '(?<=TEST: )[^ ]+' file.yml)
+EMAIL=$(grep -oP '(?<=EMAIL: )[^ ]+' file.yml)
+EMAIL_PASSWORD=$(grep -oP '(?<=EMAIL_PASSWORD: )[^ ]+' file.yml)
+
+echo "TEST=$TEST" > .env
+echo "EMAIL=$EMAIL" >> .env
+echo "EMAIL_PASSWORD=$EMAIL_PASSWORD" >> .env
